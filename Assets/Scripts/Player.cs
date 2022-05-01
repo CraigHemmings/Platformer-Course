28bad433e8cc1938fea60ea6d51944c29fd26577
Craig Hemmings <craighemmings1991@hotmail.com>
2022-05-01 14:41:49 +0100

    Added variable speed to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 1f;
 
-    // Update is called once per frame
     void Update()
     {
         // taking user input for left and right keys, and assigning them into a variable
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal") * moveSpeed;
         // storying the getcomponent rigidbody2d into a variable, this access's the rigidbody component
         Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
         // this allows the user to move by hitting the left and right arrow keys
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
     }
 }
+  
